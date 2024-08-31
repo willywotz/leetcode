e626064167d37b4de3eb0f252c364b7a2622d702
willywotz <dogajack@gmail.com>
2024-08-31 18:09:20 +0700

    add search insert position

--- src/s0035_search_insert_position.rs
+++ src/s0035_search_insert_position.rs
@@ -4,7 +4,17 @@ pub struct Solution {}
 
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        0
+        if nums.is_empty() {
+            return 0;
+        }
+
+        for i in 0..nums.len() {
+            if nums[i] >= target {
+                return i as i32;
+            }
+        }
+
+        nums.len() as i32
     }
 }
 
