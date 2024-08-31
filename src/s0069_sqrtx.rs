187aaa0d7ca551a0b04333e143d3bbf43a82e575
willywotz <dogajack@gmail.com>
2024-08-31 22:12:08 +0700

    fix sqrtx

--- src/s0069_sqrtx.rs
+++ src/s0069_sqrtx.rs
@@ -4,6 +4,8 @@ pub struct Solution {}
 
 impl Solution {
     pub fn my_sqrt(x: i32) -> i32 {
+        if x == 0 { return 0; }
+
         let mut size = x;
         let mut base = 1;
         while size > 1 {
@@ -28,5 +30,7 @@ mod tests {
         assert_eq!(2, Solution::my_sqrt(8));
         assert_eq!(46339, Solution::my_sqrt(2147395599));
         assert_eq!(46340, Solution::my_sqrt(2147395600));
+        assert_eq!(0, Solution::my_sqrt(0));
+        assert_eq!(1, Solution::my_sqrt(1));
     }
 }
