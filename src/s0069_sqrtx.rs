22d43ba3b8e42aef7510b29e800cc623113f5a35
willywotz <dogajack@gmail.com>
2024-08-31 22:07:23 +0700

    add sqrtx

--- src/s0069_sqrtx.rs
+++ src/s0069_sqrtx.rs
@@ -3,8 +3,18 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn my_sqrt(_x: i32) -> i32 {
-        0
+    pub fn my_sqrt(x: i32) -> i32 {
+        let mut size = x;
+        let mut base = 1;
+        while size > 1 {
+            let half = size / 2;
+            let mid = base + half;
+            if mid <= x / mid {
+                base = mid;
+            }
+            size -= half;
+        }
+        base
     }
 }
 
@@ -16,5 +26,7 @@ mod tests {
     fn test() {
         assert_eq!(2, Solution::my_sqrt(4));
         assert_eq!(2, Solution::my_sqrt(8));
+        assert_eq!(46339, Solution::my_sqrt(2147395599));
+        assert_eq!(46340, Solution::my_sqrt(2147395600));
     }
 }
