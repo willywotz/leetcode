e254218ac095d6e4e5f80dce90587606be1ba4c5
willywotz <dogajack@gmail.com>
2024-08-31 21:01:05 +0700

    add add binary

--- src/s0067_add_binary.rs
+++ src/s0067_add_binary.rs
@@ -3,8 +3,29 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn add_binary(_a: String, _b: String) -> String {
-        String::new()
+    pub fn add_binary(a: String, b: String) -> String {
+        let mut result = Vec::new();
+        let mut carry = 0u8;
+
+        let mut a_bytes = a.as_bytes().to_vec();
+        let mut b_bytes = b.as_bytes().to_vec();
+
+        a_bytes.reverse();
+        b_bytes.reverse();
+
+        for i in 0..a.len().max(b.len()) {
+            carry += if i < a_bytes.len() { a_bytes[i] - b'0' } else { 0 };
+            carry += if i < b_bytes.len() { b_bytes[i] - b'0' } else { 0 };
+            result.push((carry % 2) as u8 + b'0');
+            carry /= 2;
+        }
+
+        if carry == 1 {
+            result.push(b'1');
+        }
+
+        result.reverse();
+        String::from_utf8(result).unwrap()
     }
 }
 
