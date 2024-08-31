3cc19201a160abdebf6f7adcfc67e8df7fa3c304
willywotz <dogajack@gmail.com>
2024-08-31 22:32:09 +0700

    add climbing stairs

--- src/s0070_climbing_stairs.rs
+++ src/s0070_climbing_stairs.rs
@@ -3,8 +3,20 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn climb_stairs(_n: i32) -> i32 {
-        0
+    pub fn climb_stairs(n: i32) -> i32 {
+        if n == 1 || n == 2 {
+            return n;
+        }
+
+        let mut s = vec![0; n as usize];
+        s[0] = 1;
+        s[1] = 2;
+
+        for i in 2..n as usize {
+            s[i] = s[i - 1] + s[i - 2];
+        }
+
+        s[n as usize - 1]
     }
 }
 
@@ -16,5 +28,6 @@ mod tests {
     fn test() {
         assert_eq!(2, Solution::climb_stairs(2));
         assert_eq!(3, Solution::climb_stairs(3));
+        assert_eq!(5, Solution::climb_stairs(4));
     }
 }
